a261b4cb6aeb04bc7b85db56f1d34a4b652d374b
meeum <sam@meeum.com>
2018-09-12 13:57:12 +1000

    Initial commit

---
+++ resources/js/jquery.browser-detect.js
@@ -0,0 +1,57 @@
+var userAgent = navigator.userAgent.toLowerCase(),
+    browser   = '',
+    version   = 0;
+
+$.browser.chrome = /chrome/.test(navigator.userAgent.toLowerCase());
+
+// Is this a version of IE?
+if ($.browser.msie) {
+  userAgent = $.browser.version;
+  userAgent = userAgent.substring(0,userAgent.indexOf('.'));	
+  version = userAgent;
+  browser = "Internet Explorer";
+}
+
+// Is this a version of Chrome?
+if ($.browser.chrome) {
+  userAgent = userAgent.substring(userAgent.indexOf('chrome/') + 7);
+  userAgent = userAgent.substring(0,userAgent.indexOf('.'));	
+  version = userAgent;
+  // If it is chrome then jQuery thinks it's safari so we have to tell it it isn't
+  $.browser.safari = false;
+  browser = "Chrome";
+}
+
+// Is this a version of Safari?
+if ($.browser.safari) {
+    userAgent = userAgent.substring(userAgent.indexOf('version/') + 8);
+    userAgent = userAgent.substring(0, userAgent.indexOf('.'));
+    version = userAgent;
+    browser = "Safari";
+}
+
+// Is this a version of Mozilla?
+if ($.browser.mozilla) {
+	//Is it Firefox?
+	if (navigator.userAgent.toLowerCase().indexOf('firefox') != -1) {
+		userAgent = userAgent.substring(userAgent.indexOf('firefox/') + 8);
+		userAgent = userAgent.substring(0,userAgent.indexOf('.'));
+		version = userAgent;
+		browser = "Firefox"
+	}
+	// If not then it must be another Mozilla
+	else {
+	  browser = "Mozilla (not Firefox)"
+	}
+}
+
+// Is this a version of Opera?
+if ($.browser.opera) {
+	userAgent = userAgent.substring(userAgent.indexOf('version/') + 8);
+	userAgent = userAgent.substring(0,userAgent.indexOf('.'));
+	version = userAgent;
+	browser = "Opera";
+}
+
+// Now you have two variables, browser and version
+// which have the right info
